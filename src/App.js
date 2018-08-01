cefc013df5a6d2e395d5137150714e07de06c02c
spasea <ithespv@gmail.com>
2018-08-01 19:49:36 +0300

    add fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <div className='stag__book' style={{
+          fontSize: '17px'
+        }}>
+          Made last
+        </div>
+        <div className='stag__semibold' style={{
+          fontSize: '30px'
+        }}>
+          The Road
+        </div>
+        <div className='stag__medium' style={{
+          fontSize: '15px',
+          color: '#b6b6b6'
+        }}>
+          June 18
+        </div>
+      </div>
     )
   }
 }
