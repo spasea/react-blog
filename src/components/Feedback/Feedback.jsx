d0eb5008824e0835433c4d35ce8fea2b00c8a4b8
spasea <ithespv@gmail.com>
2018-08-03 11:23:26 +0300

    add comment functionality improved

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import FeedbackForm from './FeedbackForm'
 
+import { $axios, config } from '../../App'
+
 class Feedback extends React.Component {
+  addComment = content => {
+    if (content === '') {
+      alert('You must fill the input')
+      return Promise.reject()
+    }
+
+    return $axios.post(`/pages/${config.email}/comments`, {
+      content,
+      parent: null,
+    })
+      .then(() => {
+        alert('Comment was added')
+        // in real project we can add such state management like Mobx or Redux and manage comments list
+        // updating normally instead of reloading page, but I guess reloading is enough for a test project
+        window.location.reload()
+      })
+      .catch(e => alert(e))
+  }
+
   render() {
     return (
       <section className='comments-feedback'>
@@ -9,7 +30,7 @@ class Feedback extends React.Component {
           Leave comment:
         </h2>
 
-        <FeedbackForm/>
+        <FeedbackForm callback={this.addComment}/>
       </section>
     )
   }
